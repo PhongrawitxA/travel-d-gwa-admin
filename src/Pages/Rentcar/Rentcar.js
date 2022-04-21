17d0748cfb05a68703037158037046e0a11aa906
wattanasiri <bright.wattana@gmail.com>
2022-04-21 23:04:28 +0700

    Create info page and action
    Create Activity, Rentcar, Shuttle info page and action of each page.

--- src/Pages/Rentcar/Rentcar.js
+++ src/Pages/Rentcar/Rentcar.js
@@ -3,15 +3,28 @@ import axios from 'axios'
 
 import { COLUMNS } from './Columns'
 import { Table } from '../../Components/Table/Table'
-
+import { RentcarAction } from '../../Components/Action/RentcarAction'
 
 export const Rentcar = () => {
 
-  const [data,setData] = useState([]); 
+  const [data, setData] = useState([]); 
+  const getUser = () => {
+      axios({
+          method : "GET",
+          url: "http://localhost:8080/admin/getuser",
+        }).then( res => {
+              setData(res.data);
+        });
+  }
+  useEffect(()=> {
+      (async () => {
+          await getUser();
+      })();
+  },[]); 
 
   return (
     <div>
-        <Table header={'พาร์ทเนอร์เช่ารถ'} data={data} column={COLUMNS} />                   
+        <Table header={'พาร์ทเนอร์เช่ารถ'} data={data} column={COLUMNS} Action={RentcarAction}/>                   
     </div>
   )
 }
--- src/Pages/Shuttle/Shuttle.js
+++ src/Pages/Shuttle/Shuttle.js
@@ -3,15 +3,28 @@ import axios from 'axios'
 
 import { COLUMNS } from './Columns'
 import { Table } from '../../Components/Table/Table'
-
+import { ShuttleAction } from '../../Components/Action/ShuttleAction'
 
 export const Shuttle = () => {
 
-  const [data,setData] = useState([]); 
+  const [data, setData] = useState([]); 
+  const getUser = () => {
+      axios({
+          method : "GET",
+          url: "http://localhost:8080/admin/getuser",
+        }).then( res => {
+              setData(res.data);
+        });
+  }
+  useEffect(()=> {
+      (async () => {
+          await getUser();
+      })();
+  },[]);
 
   return (
     <div>
-        <Table header={'พาร์ทเนอร์รถ รับ-ส่ง'} data={data} column={COLUMNS} />                   
+        <Table header={'พาร์ทเนอร์รถ รับ-ส่ง'} data={data} column={COLUMNS} Action={ShuttleAction} />                   
     </div>
   )
 }
