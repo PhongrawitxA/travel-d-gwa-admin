035a3232082aba054b090de36c2ee1abf9e9b9c5
wattanasiri <bright.wattana@gmail.com>
2022-05-12 00:33:27 +0700

    Edit CSS receipt page

--- src/Components/Action/ReceiptAction/ReceiptRentcarAction.js
+++ src/Components/Action/ReceiptAction/ReceiptRentcarAction.js
@@ -1,6 +1,6 @@
 import React ,{ useState } from 'react'
 import axios from "axios";
-import './ReceiptRentcarAction.css'
+import './ReceiptAction.css'
 
 import { Button, Modal, render} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
